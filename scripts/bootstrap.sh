1adba75882ec5858b6c485c6cc131d2a6f85ca9d
Fabio Rocha <frock81@yahoo.com.br>
2020-02-03 18:04:41 -0300

    Update bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
-# Automatic restart services.
+
+# To fix dpkg error in provisioning.
+export DEBIAN_FRONTEND=noninteractive
+
+# Avoid prompting questions.
 echo '* libraries/restart-without-asking boolean true' \
   | debconf-set-selections
-# First provision
-if [ ! -e /usr/bin/python ]; then
-	apt-get update --fix-missing
-	apt-get install -yq python-minimal
+
+# Use recent packages.
+if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -1440)" ]; then
+  apt update --fix-missing && apt -yq upgrade
 fi
-test -e /usr/bin/pip || apt-get install -yq python-pip
-test -e /usr/bin/ansible || pip install ansible
-PROVISION_PATH=/vagrant/provision
-# Install roles only when there is a list.
-if (grep -E -- "^- " $PROVISION_PATH/requirements.yml > /dev/null); then
-	ansible-galaxy install \
-	    -r $PROVISION_PATH/requirements.yml \
-	    -p $PROVISION_PATH/roles
+
+# In the first vagrant up, python wont be installed. We use this characteristic
+# to update apt sources file.
+if ! test -e /usr/bin/python; then
+  sed -i 's#http://archive.ubuntu.com#http://br.archive.ubuntu.com#g' \
+    /etc/apt/sources.list
+  apt update
+  apt install -yq python-minimal
 fi
+
+# Install Ansible via Python PIP.
+test -e /usr/bin/pip || apt install -yq python-pip
+test -e /usr/bin/ansible-galaxy || pip install ansible
+
+# Install required roles.
+ansible-galaxy install \
+    -r /vagrant/provision/requirements.yml \
+    -p /vagrant/provision/roles \
+    --ignore-errors
